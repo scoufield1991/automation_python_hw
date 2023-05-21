b09074c365893cda3e0cdb7bd360635383ee998d
yuriikondrashev <scoufield1991@gmail.com>
2023-05-21 21:05:24 +0300

    fix comments

--- hw4/hw4_task2.py
+++ hw4/hw4_task2.py
@@ -1,3 +1,4 @@
+import re
 """
 you have a list of variable names in camel case format ["FirstItem", "FriendsList", "MyTuple"]
 convert it to a list of variable names for python in snake case format ["first_item", "friends_list", "my_tuple"]
@@ -5,12 +6,9 @@ convert it to a list of variable names for python in snake case format ["first_i
 
 default_list = ["FirstItem", "FriendsList", "MyTuple"]
 converting_list = []
-for i in default_list:
-    count = 0
-    for j in i:
-        if not j == j.lower():
-            count += 1
-            if count > 1:
-                i = (i[:i.index(j)] + '_' + i[i.index(j):]).lower()
-                converting_list.append(i)
-print(f'Convert list: {converting_list}')
+
+for word in default_list:
+    split_words = re.findall(r'[A-Z][^A-Z]*', word)
+    converting_list.append(split_words[0].lower() + '_' + split_words[1].lower())
+print(converting_list)
+
